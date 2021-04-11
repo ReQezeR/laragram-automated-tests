d1f4161996ca7d91164b528437f891aa1cc2bc72
ReQezeR <mp62639@o2.pl>
2021-04-11 22:24:36 +0200

    init homepage

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -4,8 +4,25 @@ from Pages.BasePage import BasePage
 
 
 class HomePageLocators:
-    EXAMPLE_LOCATOR = (By.CLASS_NAME, 'css-34e4ux')
+    SHARE_PHOTO_PAGE_LINK = (By.LINK_TEXT, "//*[@title='New post']")
+    YOUR_PROFILE_PAGE_LINK = (By.LINK_TEXT, "//*[@title='Your Profile']")
+    SETTINGS_PAGE_LINK = (By.LINK_TEXT, "//*[@title='Settings']")
+    LOGOUT_LINK = (By.XPATH, "//*[@title='Logout']")
 
 
 class HomePage(BasePage):
-    pass
+    def logout(self):
+        logout_button = self.driver.find_element(*HomePageLocators.LOGOUT_LINK)
+        logout_button.click()
+
+    def go_to_share_photo_page(self):
+        share_photo_button = self.driver.find_element(*HomePageLocators.SHARE_PHOTO_PAGE_LINK)
+        share_photo_button.click()
+
+    def go_to_settings_page(self):
+        settings_button = self.driver.find_element(*HomePageLocators.SETTINGS_PAGE_LINK)
+        settings_button.click()
+
+    def go_to_profile_page(self):
+        profile_button = self.driver.find_element(*HomePageLocators.YOUR_PROFILE_PAGE_LINK)
+        profile_button.click()
