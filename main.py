94dd319cb08af4e673b2936138730df993808b31
ReQezeR <mp62639@o2.pl>
2021-04-11 22:28:46 +0200

    add user register loop

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+from Pages.HomePage import HomePage
 from Pages.LoginPage import LoginPage
 from Pages.MainPage import MainPage
 
 
-def login_user(driver):
+def register_user(driver, username, email, password):
     driver.get("http://laragram.ml/")
     mainPage = MainPage(driver, debug=False)
-    mainPage.register()
+    mainPage.register(username, email, password)
+    homePage = HomePage(driver, debug=False)
+    homePage.logout()
 
 
-def register_user(driver):
+def login_user(driver):
     driver.get("http://laragram.ml/")
     mainPage = MainPage(driver, debug=False)
     mainPage.go_to_login_page()
@@ -22,8 +27,12 @@ def register_user(driver):
 if __name__ == '__main__':
     options = webdriver.ChromeOptions()
     options.add_argument("-incognito")
-    # options.add_argument('headless')
+    options.add_argument('headless')
 
     driver = webdriver.Chrome("./drivers/chromedriver", options=options)
-    
+    # for i in range(100):
+    #     register_user(driver, username="testowy"+str(i), email="testowy"+str(i)+"@testowo.com", password="TestTest123")
+    #     print(i)
+
+    sleep(10)
     driver.close()
