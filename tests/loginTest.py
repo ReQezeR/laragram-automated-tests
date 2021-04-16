c14cd53c7d9149a510b3b42524e549e6126bd1b6
ReQezeR <mp62639@o2.pl>
2021-04-16 16:57:46 +0200

    Create loginTest.py

---
+++ tests/loginTest.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import time
+import unittest
+
+from Pages.HomePage import HomePage
+from Pages.LoginPage import LoginPage
+from Pages.MainPage import MainPage
+from Pages.PostPage import PostPage
+
+
+class LoginTest(unittest.TestCase):
+    debug = False
+    @classmethod
+    def setUpClass(cls):
+        options = webdriver.ChromeOptions()
+        options.add_argument("-incognito")
+        # options.add_argument('headless')
+        cls.driver = webdriver.Chrome("../drivers/chromedriver", options=options)
+        cls.driver.implicitly_wait(5)
+
+    def test_login_with_good_data(self, login="agent00", email="agent00@testowo.com", password="TestTest123"):
+        driver = self.driver
+        driver.get("http://laragram.ml/")
+        mainPage = MainPage(driver, debug=self.debug)
+        mainPage.header.go_to_login_page()
+        loginPage = LoginPage(driver, debug=self.debug)
+        loginPage.login(email, password)
+        homePage = HomePage(driver, debug=self.debug)
+        homePage.checkUser(login) #sprawdzenie czy poprawnie zalogowano
+        homePage.header.logout()
+
+    def test_login_with_wrong_data(self, login="brak", email="brak@brak.com", password="TestTest123"):
+        driver = self.driver
+        driver.get("http://laragram.ml/")
+        mainPage = MainPage(driver, debug=self.debug)
+        mainPage.header.go_to_login_page()
+        loginPage = LoginPage(driver, debug=self.debug)
+        loginPage.login(email, password)
+        loginPage.checkInvalidDataFeedback() #sprawdzenie czy nie zalogowano
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
